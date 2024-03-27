e5f6c90ee5af9c8594cec0db4509b73b21ada066
Michael Sumner <mdsumner@gmail.com>
2024-03-27 00:07:31 +0000

    write the params

---
+++ data-raw/params.R
@@ -0,0 +1,12 @@
+f <- fs::dir_ls("data-raw", regexp = "fgb$")
+crs <- equi7grid::crs7()
+layers <- names(crs)
+
+## establish the dataset
+
+system(sprintf("ogr2ogr data-raw/equi7grid.gpkg %s -nln %s", f[1], layers[1]))
+
+for (i in tail(seq_along(layers), -1)) {
+  system(sprintf("ogr2ogr -update data-raw/equi7grid.gpkg %s -nln %s", f[i], layers[i]))
+
+}
