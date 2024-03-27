c10dfb92b2ec1426bf5057b9190f7a42af6aeb38
Michael Sumner <mdsumner@gmail.com>
2024-03-27 00:32:08 +0000

    _aeqd

--- data-raw/layers.R
+++ data-raw/layers.R
@@ -4,9 +4,9 @@ layers <- names(crs)
 
 ## establish the dataset
 
-system(sprintf("ogr2ogr data-raw/equi7grid.gpkg %s -nln %s", f[1], layers[1]))
+system(sprintf("ogr2ogr data-raw/equi7grid_aeqd.gpkg %s -nln %s", f[1], layers[1]))
 
 for (i in tail(seq_along(layers), -1)) {
-  system(sprintf("ogr2ogr -update data-raw/equi7grid.gpkg %s -nln %s", f[i], layers[i]))
+  system(sprintf("ogr2ogr -update data-raw/equi7grid_aeqd.gpkg %s -nln %s", f[i], layers[i]))
 
 }
